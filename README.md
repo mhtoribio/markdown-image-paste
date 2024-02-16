28c0bf239c1ef65ce4eefcda06319088b39ca869
Frederik Hansen <frederik@altofte.dk>
2024-02-16 09:17:34 +0100

    MVP added readme

---
+++ README.md
@@ -0,0 +1,43 @@
+# :warning: this is an pre alpha MVP plugin, i made it in 5 hours!
+# Markdown-image-paste
+Markdown-image-paste is a neovim plugin, that saves an in your clipboard to a folder local to your markdown file, and then includes it in said markdown file.
+
+# Usage
+don't
+
+but if you do:
+
+install using whatever plugin manager you want.
+
+### Lazy
+```
+require('lazy').setup({
+    'fred441a/markdown-image-paste'
+})
+```
+
+### Packer
+```
+use 'fred441a/markdown-image-paste'
+```
+
+### plug
+```
+Plug 'fred441a/markdown-image-paste'
+```
+
+
+## config
+use code snippet to bind to <C-i>
+```lua
+vim.api.nvim_set_keymap('n','<C-i>', "<cmd>lua require('markdown-image-paste').pasteImage()<CR>", {noremap = true})
+```
+
+
+# Future feature list
+ - [ ] check if data in clipboard is image before pasting
+ - [ ] check if file is markdown file
+ - [ ] check if image in clipboard already has been saved to file before ( reuse old files )
+ - [ ] file name collision detection and resolution
+ - [ ] promt user for custom file name
+ - [ ] support for other rich text formats (fx latex, RTF)
--- lua/markdown-image-paste/init.lua
+++ lua/markdown-image-paste/init.lua
@@ -1,8 +1,40 @@
-
 local M = {}
 
+M.dirname = "images"
+M.filename = "img"
+
+function M.setup(config)
+    M.dirname = config.dirname
+    M.filename = config.filename
+end
+
+function M.createImageFile()
+    math.randomseed(os.time())
+    local filename = M.filename..math.random(1,9999)..'.png'
+    local filepath = vim.fn.expand('%:h')..'/'..M.dirname..'/'.. filename
+    image = io.open(filepath,"w")
+    for key,imgpart in pairs(vim.fn.getreg('+',1,1)) do
+        image:write(imgpart:gsub('\n','\0').."")
+        image:write('\n')
+    end
+    image:close()
+    return filename
+end
+
+local function insertText(text)
+    local current_line = vim.api.nvim_win_get_cursor(0)[1]
+    local current_col = vim.api.nvim_win_get_cursor(0)[2]
+    local current_buf = vim.api.nvim_get_current_buf()
+
+    -- Insert the text at the cursor position
+    vim.api.nvim_buf_set_text(current_buf, current_line - 1, current_col, current_line - 1, current_col, {text})
+end
+
 function M.pasteImage()
-    vim.print(getreg('+'))
+    vim.print("mkdir -p " .. vim.fn.expand('%:h').."/"..M.dirname..'/' )
+    os.execute("mkdir -p " .. vim.fn.expand('%:h').."/"..M.dirname..'/' )
+    local imgName = M.createImageFile()
+    insertText("!["..imgName.."]("..M.dirname.."/"..imgName..")")
 end
 
 return M
